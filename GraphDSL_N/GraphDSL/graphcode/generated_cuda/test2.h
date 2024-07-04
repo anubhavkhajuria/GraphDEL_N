64196561a737a261183f32746c48bfb0f248ce4f
anubhavkhajuria <62196748+anubhavkhajuria@users.noreply.github.com>
2024-07-04 10:48:12 +0530

    Update test2.h

--- GraphDSL_N/GraphDSL/graphcode/generated_cuda/test2.h
+++ GraphDSL_N/GraphDSL/graphcode/generated_cuda/test2.h
@@ -1,6 +1,6 @@
 // FOR BC: nvcc bc_dsl_v2.cu -arch=sm_60 -std=c++14 -rdc=true # HW must support CC 6.0+ Pascal or after
-#ifndef GENCPP_TEST2_H
-#define GENCPP_TEST2_H
+#ifndef GENCPP_TEST_H
+#define GENCPP_TEST_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
@@ -9,15 +9,37 @@
 #include "../libcuda.cuh"
 #include <cooperative_groups.h>
 
-void test1(graph& g,float* features,int* labels);
+void forward_prop(graph& g,float* features,int* labels,int layer
+);
+
+__device__ int layer ;
+
+
+__device__ int bajkd ; // DEVICE ASSTMENT in .h
+
+__global__ void forward_prop_kernel(int V, int E, int* d_meta, int* d_data,int* d_labels){ // BEGIN KER FUN via ADDKERNEL
+  float num_nodes  = V;
+  unsigned v = blockIdx.x * blockDim.x + threadIdx.x;
+  if(v >= V) return;
+  for (int edge = d_meta[v]; edge < d_meta[v+1]; edge++) { // FOR NBR ITR 
+    int n = d_data[edge];
+    if (d_labels[n] == d_labels[nod]){ // if filter begin 
+
+    } // if filter end
+
+  } //  end FOR NBR ITR. TMP FIX!
+} // end KER FUNC
+void Backward_prop(int* xyzzz);
+
+
+
+void run(graph& g,float* features,int* labels);
 
 
 
 __device__ int num_epoch ; // DEVICE ASSTMENT in .h
 
 __device__ int num_layer ; // DEVICE ASSTMENT in .h
-
-__device__ int bajkd ; // DEVICE ASSTMENT in .h
 
 __device__ int x ; // DEVICE ASSTMENT in .h
 
